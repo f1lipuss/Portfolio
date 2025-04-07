61f6a194f402079f72f266a61791d4718d61e8c3
f1lipuss <fairout60@gmail.com>
2025-04-07 20:01:22 +0200

    Delete pl/filipus/filipusBackupy directory

--- pl/filipus/filipusBackupy/data/PlayerBackup.kt
+++
@@ -1,9 +0,0 @@
-package pl.filipus.filipusBackupy.data
-
-import org.bukkit.inventory.ItemStack
-
-data class PlayerBackup(
-    val timestamp: Long,
-    val inventoryContents: Array<ItemStack?>,
-    val armorContents: Array<ItemStack?>
-)
